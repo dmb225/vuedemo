69342e182e369b80832245e321af270ed8ca159f
Dao Malick Benin <daomalick225@gmail.com>
2025-08-04 17:16:53 +0200

    Use a style-resources-loader plugin to automatically import styles and make them available in every component in our application

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/variables.scss'),
+      ],
+    })
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type =>
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    )
+  }
 })
